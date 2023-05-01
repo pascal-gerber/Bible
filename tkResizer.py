a50b0d9bf3df47afbe8137a5d838d46ec2adc40d
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2023-05-01 23:09:55 +0200

    tiny fix

---
+++ tkResizer.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import sys
+
+def calibrate():
+    root = tk.Tk()
+
+    SCwidth = root.winfo_screenwidth()
+    SCheigt = root.winfo_screenheight()
+
+    Screensize = ([SCwidth, SCheigt])
+    root.destroy()
+    return(Screensize)
+
+def adapt(size):
+    OS = sys.platform
+    if OS == "linux":
+        return(calibrate()[0]//200)
+    else:
+        return(size)
+
